5e176ca30df762b332f358813faa3f08fa4655bf
Nicolas Dinh <Nickydinhh@gmail.com>
2017-04-23 18:55:49 -0500

    Create main.inc.php

---
+++ main.inc.php
@@ -0,0 +1,68 @@
+<?php
+
+$nav ="<nav class="main-nav">
+ <img src="logo.png">
+ <div class="navbar">
+ <div class="dropdown">
+ <a href="#brands" onclick="myFunction()" class="dropbtn">Designers</a>
+  <div id="myDropdown" class="dropdown-content">
+   <div class="brands">
+    <a href="#home">adidas</a>
+    <a href="#about">A Bathing Ape</a>
+    <a href="#contact">Gosha Rubchinsky</a>
+    <a href="#brands">All Designers</a>
+    </div>
+    <div class="brands">
+    <a href="#home">Yeezy</a>
+    <a href="#about">Human Made</a>
+    <a href="#contact">D.T.T.K.</a>
+    </div>
+    <div class="brands">
+    <a href="#home">Stone Island</a>
+    <a href="#about">Rockit</a>
+    <a href="#contact">Stussy</a>
+    </div>
+     </div>
+     </div>
+  <a href="#brands" onclick="myFunction2()" class="dropbtn2">Shop</a>
+  <div id="myDropdown2" class="dropdown-content2">
+   <div class="shop">
+    <a href="#home">Accessories</a>
+    <a href="#about">Tops</a>
+    <a href="#contact">Bottoms</a>
+    <a href="#brands">Footwear</a>
+    </div>    
+ </div>
+
+  <a class="list" href="#">Customer Care</a>
+  <a class="list" href="#">Shopping Cart</a>
+    
+
+     </div>
+    </nav>";
+
+$footer ="<footer>
+   <div class="footernav">
+    <ul>
+        <li><h3>Navigation</h3></li>
+         <li><a href="">Designers</a></li>
+         <li><a href="">Shop</a></li>
+         <li><a href="">Customer Care</a></li>
+     </ul> 
+     <ul>
+        <li><h3>Information</h3></li>
+         <li><a href="">Privacy Policy</a></li>
+         <li><a href="">Terms Of Service</a></li>
+         <li><a href="">Return Policy</a></li>
+     </ul> 
+     <ul>
+        <li><h3>Social Media</h3></li>
+         <li>Facebook</li>
+         <li>Instagram</li>
+         <li>Twitter</li>
+     </ul> 
+      </div>
+     <h6>THIS IS FOR USE FOR AARON AND NICKY'S CTEC4309 PROJECT</h6>
+  </footer>";
+
+?>
