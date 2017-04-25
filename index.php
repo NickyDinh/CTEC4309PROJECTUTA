f2f9064e28b416133cde60a46243599ef12b9cf5
Nicolas Dinh <Nickydinhh@gmail.com>
2017-04-24 19:52:34 -0500

    index.php

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+include (“main.inc.php”);
+?>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+     <link href="https://cdnjs.cloudflare.com/ajax/libs/normalize/3.0.2/normalize.min.css" rel="stylesheet" type="text/css" />
+     <link rel="stylesheet" href="index.css">
+    <title>Black Market USA </title>
+</head>
+
+     <?php print $nav ?>
+      
+<header>
+ <div class="container">
+  <img class="promo" src="slideshow_3.jpg">
+  </div>
+</header>
+
+<?php include('main.inc.php');?>
+
+<body>
+  <main>
+      
+  </main>
+    
+  <?php print $footer ?>
+   
+
+</body>
+<script src='//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+<script src="navbar.js"></script>
+
+</html>
